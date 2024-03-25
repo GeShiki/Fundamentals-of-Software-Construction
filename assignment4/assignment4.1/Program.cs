bcd41219437faf8948664fa32de002a22aa44bc2
GeShiki <1901811076@qq.com>
2024-03-26 00:04:28 +0800

    assignment4

---
+++ assignment4/assignment4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Specialized;
+
+namespace assignment4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Action<List<int>> Solution = (list => 
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine(list[i]);
+                }
+                int max = list.Max();
+                Console.WriteLine($"最大值:{max}");
+                int min = list.Min();
+                Console.WriteLine($"最小值:{min}");
+                int sum = list.Sum();
+                Console.WriteLine($"和:{sum}");
+            });
+            Solution(list);
+        }
+    }
+}
---
+++ assignment4/assignment4.2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace assignment4._2
+{
+    public delegate void TimeHandler(object sender, int i);
+    public class Clock
+    {
+        public event TimeHandler OnTime;
+        public void Time(int i)
+        {
+            Console.WriteLine($"开始计时{i}秒。");
+            OnTime(this, i);
+        }
+    }
+    public class Timer
+    {
+        public Clock clock = new Clock();
+        public Timer()
+        {
+            clock.OnTime += Tick;
+            clock.OnTime += Alarm;
+        }
+        public void Tick(object sender, int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.WriteLine("嘀嗒");
+            }
+        }
+        public void Alarm(object sender, int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (j == i - 1)
+                {
+                    Console.WriteLine("响铃");
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.clock.Time(5);
+        }
+    }
+}
